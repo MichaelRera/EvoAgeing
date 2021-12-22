45cb7bc2c5b8130cb876b15ece5a9474ca9447a4
UTELife <michael_rera@yahoo.fr>
2021-12-22 11:45:22 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
-install.packages("IBMPopSim")
-install.packages("tidyverse")
-install.packages("data.table")
+install.packages("Rcpp", dependencies = TRUE, INSTALL_opts = '--no-lock')
+install.packages("IBMPopSim", dependencies = TRUE, INSTALL_opts = '--no-lock')
+install.packages("tidyverse", dependencies = TRUE, INSTALL_opts = '--no-lock')
+install.packages("data.table", dependencies = TRUE, INSTALL_opts = '--no-lock')
